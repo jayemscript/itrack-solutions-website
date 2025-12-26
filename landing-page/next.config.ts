4806d93a6ae4bc783849024c23d04bf9e8592504
jayemscript <johnmarkpulmano052802@gmail.com>
2025-12-26 11:56:07 +0800

    fix next.config.ts

--- landing-page/next.config.ts
+++ landing-page/next.config.ts
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/uploads/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/uploads/:path*`,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
